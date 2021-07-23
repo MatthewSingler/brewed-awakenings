e8d01836cdea93972816f041142ba098fce9a9ab
Matthew Singler <matthew.singler@gmail.com>
2021-07-23 11:54:39 -0500

    Fixed some of event listener. Still doesnt work. Cant figure out last interpolation

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,12 +1,15 @@
 import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+import { getProducts } from "./database.js"
 
+const products = getProducts()
 const employees = getEmployees()
-
+const orders = getOrders()
 export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li id="employee--${employee.Id}">${employee.name}</li>`
+        html += `<li id="employee--${employee.id}">${employee.name}</li>`
     }
 
     html += "</ul>"
@@ -14,3 +17,27 @@ export const Employees = () => {
     return html
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [, employeeId] = itemClicked.id.split("--")
+
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    const employeeOrders = orders.filter(
+                        (order) => {
+                            if (order.employeeId === employeeId) {
+                                return true
+                            }
+                        }
+                    )
+                    window.alert (`${employee.name} has sold ${employeeOrders} products `)
+                }
+                
+            }
+        }
+    }
+
+)
